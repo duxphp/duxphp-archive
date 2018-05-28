85219eb3e6f35be4c279d9142fc5ec5b402046b0
Mr.L <349865361@qq.com>
2018-05-28 14:09:47 +0800

    no message

--- src/kernel/Function.php
+++ src/kernel/Function.php
@@ -594,8 +594,11 @@ function log_no($pre = '') {
  * @return string
  */
 function load_ui($path = '') {
-    $css = 'https://cdn.duxphp.com/duxjs/css/dux.css?v=1.0.1';
-    $js = 'https://cdn.duxphp.com/duxjs/js/dux.min.js?v=1.0.1';
+    $config = \dux\Config::get('dux.use');
+    if($config['ui_local']) {
+        $css = 'https://cdn.duxphp.com/duxjs/css/dux.css?v=1.0.2';
+        $js = 'https://cdn.duxphp.com/duxjs/js/dux.min.js?v=1.0.2';
+    }
     $data = [];
     $data[] = '<link rel="stylesheet" href="' . $css . '">'."\r\n";
     $data[] = '<script type="text/javascript" src="' . $js . '" data-cfg-autoload="false" data-path="' . $path . '/" data-role="' . ROLE_NAME . '" data-root="' . ROOT_URL . '"></script>'."\r\n";
