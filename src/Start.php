0ffbf7a575a2de5f0b3345a9bc65dd478de192dd
Mr.L <admin@duxphp.com>
2019-04-12 15:49:35 +0800

    no message

--- src/Start.php
+++ src/Start.php
@@ -47,8 +47,8 @@ class Start {
             echo 'Please define ROOT_PATH constants';
             exit;
         }
-        if (!defined('VERSION')) define('VERSION', '1.1.13');
-        if (!defined('VERSION_DATE')) define('VERSION_DATE', '20190410');
+        if (!defined('VERSION')) define('VERSION', '1.1.14');
+        if (!defined('VERSION_DATE')) define('VERSION_DATE', '20190412');
         if (!defined('IS_CLI')) define('IS_CLI', preg_match("/cli/i", php_sapi_name()) ? true : false);
         if (!defined('URL')) define('URL', $_SERVER['REQUEST_URI']);
         if (!defined('START_TIME')) define('START_TIME', microtime());
