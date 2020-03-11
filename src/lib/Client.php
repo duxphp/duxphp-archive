32082b8da1efb43ec4b15e181c3230f9a8c110aa
Mr.L <admin@duxphp.com>
2020-03-11 20:15:35 +0800

    no message

--- src/lib/Client.php
+++ src/lib/Client.php
@@ -28,10 +28,11 @@ class Client {
             $ip = getenv('HTTP_X_FORWARDED_FOR');
             $ips = explode(',', $ip);
             $ip = $ips[0];
+        } elseif (getenv('REMOTE_ADDR')) {
+            $ip = getenv('REMOTE_ADDR');
         } else {
             $ip = '0.0.0.0';
         }
-        return $ip;
     }
 
     /**
